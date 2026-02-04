e557cc22cbc44e70448f5fbd2fd2fd2817e98239
amirdeljouyi <amirdeljouyi@gmail.com>
2026-02-04 14:13:46 +0100

    move the old codes (comparison and the old dataset) to legacy

--- dataset/coverage_compile.sh
+++
@@ -1,9 +0,0 @@
-SUT_JAR="tmp/quarkus_classes"
-TEST_SRC_DIR="$1"
-TEST_CLASSES_DIR="$2"
-LIBS_DIR="libs/*"
-
-javac \
-  -cp "${LIBS_DIR}:${SUT_JAR}" \
-  -d "${TEST_CLASSES_DIR}" \
-  $(find "${TEST_SRC_DIR}" -name "*.java")
--- dataset/run_them.sh
+++
@@ -1,4 +0,0 @@
-chmod +x run_manual.sh run_agt.sh
-
-./run_manual.sh projects.csv
-./run_agt.sh projects.csv
